b8b019c6138b8e82317ba63a9447b9397ae849ed
minomio <68199057+minomio@users.noreply.github.com>
2022-08-10 18:15:37 -0400

    Create dalleMini_ImageGen.py

---
+++ dalleMini_ImageGen.py
@@ -0,0 +1,157 @@
+
+# Install required libraries
+#pip3 install jaxlib
+#pip3 install -q git+https://github.com/huggingface/transformers.git
+#pip3 install -q git+https://github.com/patil-suraj/vqgan-jax.git
+#pip3 install -q git+https://github.com/borisdayma/dalle-mini.git
+
+
+# Model references
+
+# dalle-mini
+DALLE_MODEL = "dalle-mini/dalle-mini/wzoooa1c:latest"  # can be wandb artifact or 🤗 Hub or local folder or google bucket
+DALLE_COMMIT_ID = None
+
+# VQGAN model
+VQGAN_REPO = "dalle-mini/vqgan_imagenet_f16_16384"
+VQGAN_COMMIT_ID = "e93a26e7707683d349bf5d5c41c5b0ef69b677a9"
+
+# CLIP model
+CLIP_REPO = "openai/clip-vit-large-patch14"
+CLIP_COMMIT_ID = None
+
+
+import jax
+import jax.numpy as jnp
+
+# check how many devices are available
+jax.local_device_count()
+
+
+# Load models & tokenizer
+from dalle_mini import DalleBart, DalleBartProcessor
+from vqgan_jax.modeling_flax_vqgan import VQModel
+from transformers import CLIPProcessor, FlaxCLIPModel
+
+# Load dalle-mini
+model = DalleBart.from_pretrained(DALLE_MODEL, revision=DALLE_COMMIT_ID)
+
+# Load VQGAN
+vqgan = VQModel.from_pretrained(VQGAN_REPO, revision=VQGAN_COMMIT_ID)
+
+# Load CLIP
+clip = FlaxCLIPModel.from_pretrained(CLIP_REPO, revision=CLIP_COMMIT_ID)
+clip_processor = CLIPProcessor.from_pretrained(CLIP_REPO, revision=CLIP_COMMIT_ID)
+
+
+
+from flax.jax_utils import replicate
+
+model._params = replicate(model.params)
+vqgan._params = replicate(vqgan.params)
+clip._params = replicate(clip.params)
+
+
+
+from functools import partial
+
+# model inference
+@partial(jax.pmap, axis_name="batch", static_broadcasted_argnums=(3, 4, 5, 6))
+def p_generate(
+    tokenized_prompt, key, params, top_k, top_p, temperature, condition_scale
+):
+    return model.generate(
+        **tokenized_prompt,
+        prng_key=key,
+        params=params,
+        top_k=top_k,
+        top_p=top_p,
+        temperature=temperature,
+        condition_scale=condition_scale,
+    )
+
+
+# decode image
+@partial(jax.pmap, axis_name="batch")
+def p_decode(indices, params):
+    return vqgan.decode_code(indices, params=params)
+
+
+# score images
+@partial(jax.pmap, axis_name="batch")
+def p_clip(inputs, params):
+    logits = clip(params=params, **inputs).logits_per_image
+    return logits
+
+import random
+
+# create a random key
+seed = random.randint(0, 2**32 - 1)
+key = jax.random.PRNGKey(seed)
+
+from dalle_mini import DalleBartProcessor
+processor = DalleBartProcessor.from_pretrained(DALLE_MODEL, revision=DALLE_COMMIT_ID)
+
+descript = "many clouds inside a computer drinking tea"
+tokenized_prompt = processor([descript])
+tokenized_prompt = replicate(tokenized_prompt)
+
+# number of predictions
+n_predictions = 1
+
+# We can customize top_k/top_p used for generating samples
+gen_top_k = None
+gen_top_p = None
+temperature = 0.85
+cond_scale = 3.0
+
+
+from flax.training.common_utils import shard_prng_key
+import numpy as np
+from PIL import Image
+from tqdm.notebook import trange
+
+# generate images
+images = []
+for i in trange(max(n_predictions // jax.device_count(), 1)):
+    # get a new key
+    key, subkey = jax.random.split(key)
+    # generate images
+    encoded_images = p_generate(
+        tokenized_prompt,
+        shard_prng_key(subkey),
+        model.params,
+        gen_top_k,
+        gen_top_p,
+        temperature,
+        cond_scale,
+    )
+    # remove BOS
+    encoded_images = encoded_images.sequences[..., 1:]
+    # decode images
+    decoded_images = p_decode(encoded_images, vqgan.params)
+    decoded_images = decoded_images.clip(0.0, 1.0).reshape((-1, 256, 256, 3))
+    for img in decoded_images:
+        images.append(Image.fromarray(np.asarray(img * 255, dtype=np.uint8)))
+
+
+from flax.training.common_utils import shard
+
+# get clip scores
+clip_inputs = clip_processor(
+    text=[descript] * jax.device_count(),
+    images=images,
+    return_tensors="np",
+    padding="max_length",
+    max_length=77,
+    truncation=True,
+).data
+logits = p_clip(shard(clip_inputs), clip.params)
+logits = logits.squeeze().flatten()
+
+
+
+print(f"Prompt: {descript}\n")
+for idx in logits.argsort()[::-1]:
+    display(images[idx])
+    print(f"Score: {logits[idx]:.2f}\n")
